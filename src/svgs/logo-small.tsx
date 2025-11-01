2cce09262f28508e8171d51bda97ba244de7b49e
KairoNX <behosh304@gmail.com>
2025-11-01 15:59:08 +0500

    Update logo size to 24/32 and make it white

--- src/svgs/logo-small.tsx
+++ src/svgs/logo-small.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export const LogoSmall = () => {
   return (
-    <div className="h-10 w-auto">
+    <div className="h-24 w-auto md:h-32">
       <Image
         src="/zenus-logo-full.png"
         alt="Zenus"
-        width={120}
-        height={40}
-        className="h-full w-auto object-contain"
+        width={300}
+        height={96}
+        className="h-full w-auto object-contain brightness-0 invert"
         priority
       />
     </div>
